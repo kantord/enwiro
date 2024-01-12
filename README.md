9e28e96e387e7df226151cfbe54c56ef699235c4
Daniel Kantor <git@daniel-kantor.com>
2024-01-12 21:15:26 +0100

    add some stuff

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
- # enwiro
+# enwiro
 
 
- Enwiro is the successor to `i3-env`.
+Enwiro is the successor to `i3-env`.
+ 
+ 
+## Concepts
+ 
+### Environment
+
+An `enwiro` is a local folder or a symbolic link pointing to a folder.
+
+An environment serves as a working directory for your applications,
+such as your terminal or your code editor.
+
+An environment could be linked to:
+
+* Any branch of a Git repository checked out on your local computer
+* A folder on a remote computer
+* Any folder on your computer
+
+
+### Recipe
+
+<p align="center">
+  <img src="recipes.png" />
+</p>
+
+Recipes are automatically generated blueprints for environments.
+
+While they do not exist as environments on your computer yet, you can
+interact with them as if they were environments and when you do so,
+they will be created on the fly for you.
+
+Recipes can have a hierarchical nature. For instance, the recipe for a
+Git repository might refer to the main working tree of the Git repository,
+and serve as the "parent recipe" to recipes for creating new worktrees for
+the same Git repository.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
+use clap::Parser;
+use serde_derive::{Deserialize, Serialize};
+use std::env;
+use std::fs;
+use std::fs::create_dir;
+use std::path::Path;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ConfigurationValues {
+    workspaces_directory: String,
+}
+
+impl ::std::default::Default for ConfigurationValues {
+    fn default() -> Self {
+        let home_dir = env::home_dir().expect("User home directory not found");
+        let default_workspaces_directory = home_dir.join(".enwiro_envs");
+
+        Self {
+            workspaces_directory: default_workspaces_directory.to_str().unwrap().to_string(),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum EnvironmentType {
+    Simple,
+}
+
+#[derive(Debug)]
+struct Environment {
+    path: String,
+    name: String,
+    type_: EnvironmentType,
+}
+
+fn get_basic_environments(source_directory: &str) -> Vec<Environment> {
+    let mut results: Vec<Environment> = vec![];
+    let directory_entries = fs::read_dir(source_directory).expect("Could not read workspaces directory. Make sure that the path is a directory you have permissions to access.");
+
+    for directory_entry in directory_entries {
+        let path = directory_entry.unwrap().path();
+
+        if path.is_dir() {
+            results.push(Environment {
+                path: path.to_str().unwrap().to_string(),
+                name: path.file_name().unwrap().to_str().unwrap().to_string(),
+                type_: EnvironmentType::Simple,
+            })
+        }
+    }
+
+    results
+}
+
+#[derive(Parser)]
+enum EnwiroCli {
+    ListEnvironments(ListEnvironmentsArgs),
+}
+
+#[derive(clap::Args)]
+#[command(author, version, about)]
+struct ListEnvironmentsArgs {}
+
+fn ensure_can_run(config: &ConfigurationValues) {
+    let environments_directory = Path::new(&config.workspaces_directory);
+    if !environments_directory.exists() {
+        create_dir(environments_directory)
+            .expect("Workspace directory does not exist and could not be automatically created.");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let EnwiroCli::ListEnvironments(_) = EnwiroCli::parse();
+
+    let config: ConfigurationValues =
+        confy::load("enwiro", None).expect("Configuration file must be present");
+
+    ensure_can_run(&config);
+
+    let mut environments = get_basic_environments(&config.workspaces_directory);
+
+    dbg!(config);
+    dbg!(environments);
 }
