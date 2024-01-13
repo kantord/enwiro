9a22d6eff6aa3839702993ca83369acb9ea04b03
Daniel Kantor <git@daniel-kantor.com>
2024-01-13 17:04:38 +0100

    feat: add show-path command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use serde_derive::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::fs::create_dir;
@@ -28,24 +29,29 @@ enum EnvironmentType {
 
 #[derive(Debug)]
 struct Environment {
+    id: String,
     path: String,
     name: String,
     type_: EnvironmentType,
 }
 
-fn get_basic_environments(source_directory: &str) -> Vec<Environment> {
-    let mut results: Vec<Environment> = vec![];
+fn get_environments(source_directory: &str) -> HashMap<String, Environment> {
+    let mut results: HashMap<String, Environment> = HashMap::new();
     let directory_entries = fs::read_dir(source_directory).expect("Could not read workspaces directory. Make sure that the path is a directory you have permissions to access.");
 
     for directory_entry in directory_entries {
         let path = directory_entry.unwrap().path();
+        let id = path.file_name().unwrap().to_str().unwrap().to_string();
 
         if path.is_dir() {
-            results.push(Environment {
+            let new_environment = Environment {
+                id: id.clone(),
                 path: path.to_str().unwrap().to_string(),
-                name: path.file_name().unwrap().to_str().unwrap().to_string(),
+                name: id.clone(),
                 type_: EnvironmentType::Simple,
-            })
+            };
+
+            results.insert(id.clone(), new_environment);
         }
     }
 
@@ -55,11 +61,18 @@ fn get_basic_environments(source_directory: &str) -> Vec<Environment> {
 #[derive(Parser)]
 enum EnwiroCli {
     ListEnvironments(ListEnvironmentsArgs),
+    ShowPath(ShowPathArgs),
 }
 
 #[derive(clap::Args)]
 #[command(author, version, about)]
 struct ListEnvironmentsArgs {}
+
+#[derive(clap::Args)]
+#[command(author, version, about)]
+struct ShowPathArgs {
+    environment_name: String,
+}
 
 fn ensure_can_run(config: &ConfigurationValues) {
     let environments_directory = Path::new(&config.workspaces_directory);
@@ -69,16 +82,35 @@ fn ensure_can_run(config: &ConfigurationValues) {
     }
 }
 
+fn list_environments(config: &ConfigurationValues) {
+    let environments = get_environments(&config.workspaces_directory);
+
+    for environment in environments.values() {
+        println!("{}", environment.name);
+    }
+}
+
+fn show_path(config: &ConfigurationValues, args: ShowPathArgs) {
+    let environments = get_environments(&config.workspaces_directory);
+    let selected_environment = environments
+        .get(&args.environment_name)
+        .expect("Environment not found");
+
+    println!("{}", selected_environment.path);
+}
+
 fn main() {
-    let EnwiroCli::ListEnvironments(_) = EnwiroCli::parse();
+    let args = EnwiroCli::parse();
 
     let config: ConfigurationValues =
         confy::load("enwiro", None).expect("Configuration file must be present");
 
     ensure_can_run(&config);
 
-    let mut environments = get_basic_environments(&config.workspaces_directory);
+    match args {
+        EnwiroCli::ListEnvironments(args) => list_environments(&config),
+        EnwiroCli::ShowPath(args) => show_path(&config, args),
+    }
 
     dbg!(config);
-    dbg!(environments);
 }
