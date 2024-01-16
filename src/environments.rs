b4717bc880abe76a305d81fbcef05659596111e1
Daniel Kantor <git@daniel-kantor.com>
2024-01-16 19:30:52 +0100

    refactor: make get_environments a class method

--- src/environments.rs
+++ src/environments.rs
@@ -6,23 +6,25 @@ pub struct Environment {
     pub name: String,
 }
 
-pub fn get_environments(source_directory: &str) -> HashMap<String, Environment> {
-    let mut results: HashMap<String, Environment> = HashMap::new();
-    let directory_entries = fs::read_dir(source_directory).expect("Could not read workspaces directory. Make sure that the path is a directory you have permissions to access.");
+impl Environment {
+    pub fn get_environments(source_directory: &str) -> HashMap<String, Environment> {
+        let mut results: HashMap<String, Environment> = HashMap::new();
+        let directory_entries = fs::read_dir(source_directory).expect("Could not read workspaces directory. Make sure that the path is a directory you have permissions to access.");
 
-    for directory_entry in directory_entries {
-        let path = directory_entry.unwrap().path();
-        let id = path.file_name().unwrap().to_str().unwrap().to_string();
+        for directory_entry in directory_entries {
+            let path = directory_entry.unwrap().path();
+            let id = path.file_name().unwrap().to_str().unwrap().to_string();
 
-        if path.is_dir() {
-            let new_environment = Environment {
-                path: path.to_str().unwrap().to_string(),
-                name: id.clone(),
-            };
+            if path.is_dir() {
+                let new_environment = Environment {
+                    path: path.to_str().unwrap().to_string(),
+                    name: id.clone(),
+                };
 
-            results.insert(id.clone(), new_environment);
+                results.insert(id.clone(), new_environment);
+            }
         }
-    }
 
-    results
+        results
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod environments;
 
 use clap::Parser;
 
-use environments::get_environments;
+use environments::Environment;
 use serde_derive::{Deserialize, Serialize};
 use std::env;
 use std::fs::create_dir;
@@ -56,7 +56,7 @@ fn ensure_can_run<R: Read, W: Write>(config: &CommandContext<R, W>) {
 }
 
 fn list_environments<R: Read, W: Write>(context: &mut CommandContext<R, W>) {
-    let environments = get_environments(&context.config.workspaces_directory);
+    let environments = Environment::get_environments(&context.config.workspaces_directory);
 
     for environment in environments.values() {
         println!("{}", environment.name);
@@ -64,7 +64,7 @@ fn list_environments<R: Read, W: Write>(context: &mut CommandContext<R, W>) {
 }
 
 fn show_path<R: Read, W: Write>(context: &mut CommandContext<R, W>, args: ShowPathArgs) {
-    let environments = get_environments(&context.config.workspaces_directory);
+    let environments = Environment::get_environments(&context.config.workspaces_directory);
     let selected_environment = environments
         .get(&args.environment_name)
         .expect("Environment not found");
