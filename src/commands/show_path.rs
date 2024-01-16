167c2c3414d017f7db1315e3531f05a807893e27
Daniel Kantor <git@daniel-kantor.com>
2024-01-16 20:54:01 +0100

    refactor: move show_path tests to the relevant file

--- src/commands/show_path.rs
+++ src/commands/show_path.rs
@@ -19,3 +19,38 @@ pub fn show_path<R: Read, W: Write>(context: &mut CommandContext<R, W>, args: Sh
         .write(selected_environment.path.as_bytes())
         .unwrap();
 }
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use crate::{
+        commands::show_path::{show_path, ShowPathArgs},
+        test_utils::test_utils::{context_object, FakeContext},
+    };
+
+    #[rstest]
+    fn test_show_path_when_environment_works(mut context_object: FakeContext) {
+        context_object.create_mock_environment("foobar");
+        show_path(
+            &mut context_object,
+            ShowPathArgs {
+                environment_name: "foobar".to_string(),
+            },
+        );
+
+        assert_eq!(context_object.get_output().ends_with("foobar"), true);
+    }
+
+    #[rstest]
+    #[should_panic]
+    fn test_show_path_panics_when_env_does_not_exist(mut context_object: FakeContext) {
+        context_object.create_mock_environment("existing_env");
+        show_path(
+            &mut context_object,
+            ShowPathArgs {
+                environment_name: "non_existing_env".to_string(),
+            },
+        );
+    }
+}
