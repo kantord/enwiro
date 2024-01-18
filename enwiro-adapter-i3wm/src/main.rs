4ca43866651277ae34950ae59c0236cbfff87b7c
Daniel Kantor <git@daniel-kantor.com>
2024-01-18 21:53:30 +0100

    feat: add enwiro-adapter-i3wm

---
+++ enwiro-adapter-i3wm/src/main.rs
@@ -0,0 +1,43 @@
+use clap::Parser;
+use std::io;
+use tokio_i3ipc::I3;
+
+#[derive(Parser)]
+enum EnwiroAdapterI3WmCLI {
+    GetActiveWorkspaceId(GetActiveWorkspaceIdArgs),
+}
+
+#[derive(clap::Args)]
+#[command(author, version, about)]
+pub struct GetActiveWorkspaceIdArgs {}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> io::Result<()> {
+    let args = EnwiroAdapterI3WmCLI::parse();
+
+    match args {
+        EnwiroAdapterI3WmCLI::GetActiveWorkspaceId(_) => {
+            let mut i3 = I3::connect().await?;
+            let workspaces = i3.get_workspaces().await?;
+            let focused_workspace = workspaces
+                .into_iter()
+                .find(|workspace| workspace.focused)
+                .expect("No active workspace. This should never happen.");
+            let mut environment_name: String = "".to_string();
+            let is_active_environment = focused_workspace.id.to_string() != focused_workspace.name;
+
+            if is_active_environment {
+                environment_name = focused_workspace
+                    .name
+                    .replace(":", "")
+                    .replace(&focused_workspace.num.to_string(), "")
+                    .trim()
+                    .to_string()
+            }
+
+            println!("{}", environment_name);
+        }
+    };
+
+    Ok(())
+}
