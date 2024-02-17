d03c3786191823544cfcedeef381c99fe8104b2e
Daniel Kantor <git@daniel-kantor.com>
2024-02-17 22:34:13 +0100

    feat: add basic support for lenses in i3

--- enwiro/src/context.rs
+++ enwiro/src/context.rs
@@ -5,6 +5,7 @@ use std::io::{Read, Write};
 
 pub trait EnwiroAdapterTrait {
     fn get_active_environment_name(&self) -> Result<String, std::io::Error>;
+    fn get_active_lens_name(&self) -> Result<String, std::io::Error>;
 }
 
 pub struct EnwiroAdapterExternal {
@@ -20,14 +21,31 @@ impl EnwiroAdapterTrait for EnwiroAdapterExternal {
 
         if output.status.success() {
             let stdout = String::from_utf8_lossy(&output.stdout);
-            return Ok(stdout.to_string());
+            return Ok(stdout.to_string().split(':').nth(0).unwrap().to_string());
+        } else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            panic!("Error: {}", stderr);
+        }
+    }
+
+    fn get_active_lens_name(&self) -> Result<String, std::io::Error> {
+        let output = Command::new(&self.adapter_command)
+            .arg("get-active-workspace-id")
+            .output()
+            .expect("Adapter failed to determine active lens name");
+
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            match stdout.to_string().split(':').nth(1) {
+                Some(value) => return Ok(value.to_string()),
+                None => return Ok("".to_string()),
+            }
         } else {
             let stderr = String::from_utf8_lossy(&output.stderr);
             panic!("Error: {}", stderr);
         }
     }
 }
-
 impl EnwiroAdapterExternal {
     pub fn new(adapter_name: &str) -> Self {
         Self {
@@ -43,6 +61,13 @@ impl EnwiroAdapterTrait for EnwiroAdapterNone {
         Err(std::io::Error::new(
             std::io::ErrorKind::NotFound,
             "Could not determine active environment because no adapter is configured.",
+        ))
+    }
+
+    fn get_active_lens_name(&self) -> Result<String, std::io::Error> {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Could not determine active lens because no adapter is configured.",
         ))
     }
 }
