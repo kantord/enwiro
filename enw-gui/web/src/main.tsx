b0be0b55fe57aac17b3fa9da7b5b09dcd7f3bc45
Daniel Kantor <git@daniel-kantor.com>
2026-06-12 19:13:41 +0200

    .

--- enw-gui/web/src/main.tsx
+++ enw-gui/web/src/main.tsx
@@ -1,9 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
+
+const queryClient = new QueryClient()
 
 const rootElement = document.getElementById('root')
 if (!rootElement) {
   throw new Error('root element not found')
 }
 
-createRoot(rootElement).render(<App />)
+createRoot(rootElement).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
+)
